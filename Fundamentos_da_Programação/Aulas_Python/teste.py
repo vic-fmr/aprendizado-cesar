5f45e53026da7104984a16760117765cf3c205a1
vic-fmr <vfm@poli.br>
2024-09-19 20:58:08 -0300

    Aula 01 python

---
+++ Fundamentos_da_Programação/Aulas_Python/teste.py
@@ -0,0 +1,8 @@
+carros_vendidos = int(input("Olá Vendedor! Insira o numero de carros: "))
+valor_vendas = int(input("O valor total de suas vendas: "))
+salario = int(input("O seu salário: "))
+comissao = int(input("Sua comissão: "))
+
+valor_salario = salario + (comissao*carros_vendidos) + (valor_vendas*0.05)
+
+print(valor_salario)
