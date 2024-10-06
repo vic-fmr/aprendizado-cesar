c0c669737aa9e189f1ce113ccb505b9918934adb
vic-fmr <vfm@poli.br>
2024-10-06 19:00:41 -0300

    portfólio 1.0.0

--- Introdução_á_Computação/portfolio/assets/js/click-effect.js
+++ Introdução_á_Computação/portfolio/assets/js/click-effect.js
@@ -6,6 +6,10 @@ const navItems = document.querySelectorAll(".nav-button")
 
 const classe = "clicked"
 
+window.addEventListener("DOMContentLoaded", () => {
+    inicio_botao.classList.add(classe)
+})
+
 window.addEventListener("scroll", () => {
     navItems.forEach((item) => {
         item.classList.remove(classe)
@@ -13,11 +17,11 @@ window.addEventListener("scroll", () => {
     const currentScroll = window.scrollY;
     console.log(currentScroll)
 
-    if (currentScroll > 700 && currentScroll <= 1300) {
+    if (currentScroll > 500 && currentScroll <= 1300) {
         botaoSobreMim.classList.add(classe)
     }else if (currentScroll > 1300 && currentScroll < 2000){
         projetos_botao.classList.add(classe)
-    } else if(currentScroll > 2000){
+    } else if(currentScroll >= 2000){
         contato_botao.classList.add(classe)
     } else{
         inicio_botao.classList.add(classe)
--- Introdução_á_Computação/portfolio/assets/js/scroll-effect.js
+++ Introdução_á_Computação/portfolio/assets/js/scroll-effect.js
@@ -4,14 +4,14 @@ const toggleClass = "scrolled";
 
 window.addEventListener("scroll", () => {
 
-    const currentScroll = window.scrollY;
+  const currentScroll = window.scrollY;
 
 
-        if (currentScroll > 100) {
-            header.classList.add(toggleClass);
-        } else {
-            header.classList.remove(toggleClass);
-        }
+  if (currentScroll > 100) {
+    header.classList.add(toggleClass);
+  } else {
+    header.classList.remove(toggleClass);
+  }
 });
 
 //Bloco responsável pelos scrolls da página
@@ -24,22 +24,44 @@ const sobreMimSection = document.querySelector("#sobre-mim");
 const projetosSection = document.querySelector("#projetos")
 const contatoSecton = document.querySelector("#contato")
 
-botaoSobreMim.addEventListener("click", () =>{
+botaoSobreMim.addEventListener("click", () => {
   sobreMimSection.scrollIntoView({ behavior: "smooth" });
 });
-botaoProjetos.addEventListener("click", () =>{
+botaoProjetos.addEventListener("click", () => {
   projetosSection.scrollIntoView({ behavior: "smooth" });
 });
-botaoContato.addEventListener("click", () =>{
+botaoContato.addEventListener("click", () => {
   contatoSecton.scrollIntoView({ behavior: "smooth" });
+});
+botaoInicio.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,  // Volta ao topo da página
+    behavior: "smooth"  // Rolagem suave
+  });
 });
 
 
-botaoInicio.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,  // Volta ao topo da página
-      behavior: "smooth"  // Rolagem suave
-    });
-  });
+
+function reveal() {
+  var sections = document.querySelectorAll('.section');
+  for (var i = 0; i < sections.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = sections[i].getBoundingClientRect().top;
+    var elementVisible = 150; // Margem de visibilidade
+
+    // Se o elemento estiver visível na janela de visualização
+    if (elementTop < windowHeight - elementVisible) {
+      sections[i].classList.add('active');
+    } else {
+      sections[i].classList.remove('active');
+    }
+  }
+}
+
+// Aciona a função ao rolar a página
+window.addEventListener('scroll', reveal);
+
+// Executa a função uma vez quando a página carrega
+reveal();
 
 
