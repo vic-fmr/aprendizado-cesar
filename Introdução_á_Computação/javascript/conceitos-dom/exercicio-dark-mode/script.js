46f02eb68a2f3d719ee713060d16cfa8c1598123
Lab02 <vic-fmr>
2024-10-30 21:34:30 -0300

    exercicio js

---
+++ Introdução_á_Computação/javascript/conceitos-dom/exercicio-dark-mode/script.js
@@ -0,0 +1,12 @@
+const navbar = document.getElementById("navbar")
+
+const white = "#FFF"
+const dark = "#141414"
+
+function modoDiurno(){
+    navbar.style.backgroundColor = white
+}
+
+function modoNoturno(){
+    navbar.style.backgroundColor = dark
+}
